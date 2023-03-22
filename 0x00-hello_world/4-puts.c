b3dc1ea2897d1d677baf1f1468c8d27e22573851
DarlingtonOlelewe <o.darlington@outlook.com>
2023-03-22 01:52:09 +0100

    first project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - program Entry point
+ * puts function prints argument as output
+ * Return: program returns 0 - success
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - program entry point
+ * function prints argument to the console
+ * Return: returns 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
